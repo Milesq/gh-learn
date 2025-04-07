cc35450c1e228e81f48894100517039b6956980e
Milesq <milesq@proton.me>
2025-04-07 21:07:09 +0200

    use exceptions for auth error handling

--- gh_learn/auth.py
+++ gh_learn/auth.py
@@ -1,34 +1,45 @@
-from os import environ, path
+from os import path
+from importlib.resources import files
+
 import requests
+from dotenv import dotenv_values
 
 from .utils import get_asset
+from .exceptions import AuthenticationFailure
 
 LOGIN_DEVICE_CODE_URL = 'https://github.com/login/device/code'
 ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
 
 class GHLogin:
     def __init__(self):
+        dotenv_path = files('gh_learn').joinpath('.public.env')
+        config = dotenv_values(dotenv_path)
+
+        self.gh_client_id = config['GH_CLIENT_ID']
         self.token = get_asset('token')
         if self.token is None:
             self.login()
 
-    def login(self):
-        headers = {
+    headers = {
             'Accept': 'application/json'
-        }
+    }
 
+    def login(self):
         data = {
-            'client_id': environ['GH_CLIENT_ID'],
+            'client_id': self.gh_client_id,
             'scope': 'user',
         }
 
         resp = requests.post(
             LOGIN_DEVICE_CODE_URL,
             data=data,
-            headers=headers
+            headers=self.headers
         ).json()
 
-        device_code = resp['device_code']
+        device_code = resp.get('device_code')
+
+        if not device_code:
+            raise AuthenticationFailure(resp)
 
         print('Token is missing. Login to GitHub, pase the code below and press Enter')
         print(resp['verification_uri'])
@@ -36,8 +47,11 @@ class GHLogin:
 
         input()
 
+        self.__finish_login(device_code)
+
+    def __finish_login(self, device_code):
         data = {
-            'client_id': environ['GH_CLIENT_ID'],
+            'client_id': self.gh_client_id,
             'device_code': device_code,
             'grant_type': 'urn:ietf:params:oauth:grant-type:device_code'
         }
@@ -45,19 +59,20 @@ class GHLogin:
         resp = requests.post(
             ACCESS_TOKEN_URL,
             data=data,
-            headers=headers
+            headers=self.headers
         ).json()
-
-        print(resp)
 
         self.token = resp.get('access_token')
 
         if not self.token:
-            print('Failed to login')
-            print(resp)
-            exit(1)
+            if resp.get('error') == 'authorization_pending':
+                input("It seems you didn't login yet. Press Enter once you do it\n")
+                return self.__finish_login(device_code)
+
+            raise AuthenticationFailure(resp)
 
         self.write_token()
+
 
     def write_token(self):
         f_name = path.join(path.dirname(), 'token')
--- gh_learn/gh_learn.py
+++ gh_learn/gh_learn.py
@@ -1,16 +1,14 @@
 import sys
 import random
-from dotenv import load_dotenv
+from pprint import pprint
 
 from .auth import GHLogin
 from .update_gh_status import update_gh_status
 from .exceptions import GHApiError, AuthenticationFailure
 
-load_dotenv()
-
 def main():
     try:
-        token = GHLogin().token
+        token = GHLogin()
 
         topics = ' '.join(sys.argv[1:])
         print('Learning:', topics)
@@ -21,3 +19,8 @@ def main():
         print(resp)
     except GHApiError as err:
         print('Request failed with status code:', err)
+    except KeyboardInterrupt:
+        print('\nbye')
+    except AuthenticationFailure as err:
+        print('Authentication Error\nDetails:')
+        pprint(err.args[0])
